dfee93941e33b5824ba2ad8ef26e639e6067e121
Parth-Ramani <parthramani93@gmai.com>
2024-11-01 19:40:13 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import FileExplorerContextWrappper, {
-  FileExplorerContext
-} from "./components/context/FileExplorerContext.jsx";
+import FileExplorerContextWrappper from "./components/context/FileExplorerContext.jsx";
 import FileExplorer from "./components/FileExplorer.jsx";
-import folderData from "./data/folderData.js";
 // import "./App.css";
 function App() {
   return (
